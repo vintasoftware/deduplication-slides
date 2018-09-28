9177acd1927a89e23bf46d0c65ad9a62bb6cae5c
Flávio Juvenal <flaviojuvenal@gmail.com>
2018-09-28 17:49:13 -0300

    More stable Dedupe simple example

--- dedupe/variables/custom_variables.py
+++ dedupe/variables/custom_variables.py
@@ -1,7 +1,11 @@
 import logging
 
+from dedupe.variables.latlong import LatLongType
 from dedupe.variables.string import ShortStringType
+from recordlinkage.algorithms.distance import _haversine_distance
+from recordlinkage.algorithms.numeric import _exp_sim
 import jellyfish
+import numpy as np
 
 
 logger = logging.getLogger(__name__)
@@ -14,3 +18,15 @@ class JaroWinklerType(ShortStringType):
         super().__init__(definition)
 
         self.comparator = jellyfish.jaro_winkler
+
+
+class ExpLatLongType(LatLongType):
+    type = 'ExpLatLong'
+
+    @staticmethod
+    def comparator(x, y):
+        dist = _haversine_distance(*[*x, *y])
+        return _exp_sim(
+            np.float32(dist),
+            scale=np.float32(0.1),
+            offset=np.float32(0.05))
